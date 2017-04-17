e90268e11e855eedb51727902cdfb0570b9ba7c7
Maxim220 <aero-maks@mail.ru>
2017-04-17 21:51:10 +0300

    создал репозиторий

---
+++ CubesFloat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubesFloat : MonoBehaviour {
+
+    public float speed;
+    private Vector3 target = new Vector3 ( 0, 2.5f, 0) ;
+    public float tilt;
+
+	void Update () {
+        transform.position = Vector3.MoveTowards (transform.position , target , Time.deltaTime * speed ) ;
+        if (transform.position == target && target.y != 1f)
+            target.y = 1f;
+        else if (transform.position == target && target.y == 1f)
+            target.y = 2.5f;
+        transform.Rotate(Vector3.up * Time.deltaTime * tilt);
+	}
+}
